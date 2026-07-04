e2c61b3d3d059eda0bc0bd179c7828b45a8a61af
akaki827 <jagaimo287@gmail.com>
2026-07-04 12:27:16 +0900

    camera game map background追加＆main変更

---
+++ 2026_7_4_Dxlib/Background.cpp
@@ -0,0 +1,18 @@
+
+#include "BackGround.h"
+#include "DxLib.h"
+
+//=======================================
+//èâä˙âª
+//=======================================
+void BackGround::Init()
+{
+	imageHandle = LoadGraph("img/Background.png");
+}
+//=======================================
+//ï`âÊ
+//=======================================
+void BackGround::Draw(float cameraX)
+{
+	DrawGraph(-(int)(cameraX * 0.5f), 0, imageHandle, TRUE);
+}
