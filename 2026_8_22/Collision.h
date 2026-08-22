653ee3a17dba2735cf1afcf746b10d0801b375d9
akaki827 <jagaimo287@gmail.com>
2026-08-22 12:23:27 +0900

    2026_8_22_初期化

---
+++ 2026_8_22/Collision.h
@@ -0,0 +1,8 @@
+#pragma once
+class Collision
+{
+public:
+
+	static bool IsHitRect(float left1, float top1, float right1, float bottom1,
+		float left2, float top2, float right2, float bottom2);
+};
